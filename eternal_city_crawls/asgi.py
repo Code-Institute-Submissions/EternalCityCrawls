b94f83b0ebb66dfe68f73c36cf672c700efc3aa2
Pierluca Del Buono <pierluca@amazon.com>
2020-09-04 22:58:19 +0200

    Initialize Django Project

---
+++ eternal_city_crawls/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for eternal_city_crawls project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eternal_city_crawls.settings')
+
+application = get_asgi_application()
