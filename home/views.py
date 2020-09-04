62ac1cab92d728e5f4bc22e644e32346ea92cfa7
Pierluca Del Buono <pierluca@amazon.com>
2020-09-04 23:46:56 +0200

    home page template initiated

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse("Welcome to the home page")
+    return render(
+        request,
+        'home/home.html'
+    )
