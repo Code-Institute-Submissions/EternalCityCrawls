d72104f7da337b253b23b2743a5a110bc1ab8a6d
Pierluca Del Buono <pierluca@amazon.com>
2020-09-11 08:35:48 +0200

    adding tours urls along with first basic template and view

--- eternal_city_crawls/settings.py
+++ eternal_city_crawls/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
     # my apps
     'home',
-    'tours'
+    'tours',
 
 ]
 
--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,17 @@
+
 from django.shortcuts import render
+from .models import Tour
 
 # Create your views here.
+
+
+def all_tours(request):
+    """ A view to show all tours available, including sorting and search queries """
+
+    tours = Tour.objects.all()
+
+    context = {
+        'tours': tours,
+    }
+
+    return render(request, 'tours/tours.html', context)
