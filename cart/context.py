74b69f95a770e7d4f76c52e397fce10d4b0b203e
Pierluca Del Buono <pierluca@amazon.com>
2020-09-12 21:02:53 +0200

    populating cart from session dictionary

--- cart/context.py
+++ cart/context.py
@@ -1,15 +1,29 @@
 
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from tours.models import Tour
+
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     tour_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, participants in cart.items():
+        tour = get_object_or_404(Tour, pk=item_id)
+        total += participants * tour.cost
+        cart_items.append({
+            'item_id': item_id,
+            'participants': participants,
+            'tour': tour,
+        })
+
 
     context = {
-        'bag_items': cart_items,
+        'cart_items': cart_items,
         'total': total,
         'tour_count': tour_count,
 
