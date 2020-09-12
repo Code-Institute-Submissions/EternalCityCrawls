69276829ff537990ec99403ee97d6cc0f25ef394
Pierluca Del Buono <pierluca@amazon.com>
2020-09-12 20:05:33 +0200

    adding to cart view implemented

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,19 @@ def view_cart(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """ Add Participants of the specified Tour to the cart"""
+
+    participants = int(request.POST.get('participants'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += participants
+    else:
+        cart[item_id] = participants
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
