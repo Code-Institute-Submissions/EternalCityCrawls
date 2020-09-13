b1eca42dbf03f961d44e15a5c5ce02767eff66c3
Pierluca Del Buono <pierluca@amazon.com>
2020-09-13 15:51:57 +0200

    Add increment and decrement participants button

--- cart/views.py
+++ cart/views.py
@@ -13,6 +13,8 @@ def add_to_cart(request, item_id):
     participants = int(request.POST.get('participants'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
+    # Todo include here the code to manage the time of the booking, as size in example
+    # you will need complete logic to manage day of the booking, because on cart same item must be displayed separately.
 
     if item_id in list(cart.keys()):
         cart[item_id] += participants
