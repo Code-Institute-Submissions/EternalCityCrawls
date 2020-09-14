72e9bf73f1e20d8f405d4eb132e221860aedf1eb
Pierluca Del Buono <pierluca@amazon.com>
2020-09-14 20:52:39 +0200

    implemented delete functioanlity on cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -4,15 +4,3 @@
         form.submit();
     })
 
-    // Remove item and reload on click
-    $('.remove-item').click(function(e) {
-        var csrfToken = "{{ csrf_token }}";
-        var itemId = $(this).attr('id').split('remove_')[1];
-        var url = `/cart/remove/${itemId}`;
-        var data = {'csrfmiddlewaretoken': csrfToken};
-
-        $.post(url, data)
-         .done(function() {
-             location.reload();
-         });
-    })
