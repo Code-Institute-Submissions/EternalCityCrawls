04fe27ff4ea0bed5aaec6f6f596ff3d829d1acfc
Pierluca Del Buono <pierluca@amazon.com>
2020-09-15 07:53:52 +0200

    extended level of messages in cart view

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from tours.models import Tour
@@ -13,7 +13,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add Participants of the specified Tour to the cart"""
 
-    tour = Tour.objects.get(pk=item_id)
+    tour = get_object_or_404(Tour, pk=item_id)
     participants = int(request.POST.get('participants'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -22,6 +22,8 @@ def add_to_cart(request, item_id):
 
     if item_id in list(cart.keys()):
         cart[item_id] += participants
+        messages.success(request, f"updated participants to {tour.name}")
+
     else:
         cart[item_id] = participants
         messages.success(request, f"{tour.name} added to the cart")
@@ -34,6 +36,7 @@ def add_to_cart(request, item_id):
 def update_cart(request, item_id):
     """update participants in cart"""
 
+    tour = get_object_or_404(Tour, pk=item_id)
     participants = int(request.POST.get('participants'))
 
     cart = request.session.get('cart', {})
@@ -41,8 +44,12 @@ def update_cart(request, item_id):
 
     if participants > 0:
         cart[item_id] = participants
+        messages.success(request, f"updated participants to {tour.name}")
+
+
     else:
         cart.pop(item_id)
+        messages.success(request, f"{tour.name} removed from the cart")
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -50,13 +57,16 @@ def update_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """Remove the item from the cart"""
 
+    tour = get_object_or_404(Tour, pk=item_id)
     try:
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
 
         request.session['cart'] = cart
+        messages.success(request, f"{tour.name} removed from the cart")
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f"error removing {tour.name} from the cart")
         return HttpResponse(status=500)
