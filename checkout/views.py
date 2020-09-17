896b00a7c96ae01b1a5c5551f74b030d0a2520e2
Pierluca Del Buono <pierluca@amazon.com>
2020-09-17 08:00:37 +0200

    creating template for Order Form

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('cart', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('tours'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
 
 # Create your views here.
