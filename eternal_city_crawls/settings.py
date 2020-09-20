4ddad36f79afb7d514b0d5bbb4a75cce8f719922
Pierluca Del Buono <pierluca@amazon.com>
2020-09-20 21:42:37 +0200

    setting up config variables

--- eternal_city_crawls/settings.py
+++ eternal_city_crawls/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z2m_=*bg(@ywk!mk)8^&8a8fct+&-d8mazvh$zu&%x*fphg9bd'
+os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['eternal-city-crawls.herokuapp.com', '127.0.0.1']
 
 
