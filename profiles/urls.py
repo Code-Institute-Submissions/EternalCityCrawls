a2b1ad892b44fe30301515500781000105bfbb23
Pierluca Del Buono <pierluca@amazon.com>
2020-09-20 09:47:45 +0200

    creating view to show all the booking history

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path(' booking_history/<order_number>', views.booking_history, name='booking_history'),
+
 ]
