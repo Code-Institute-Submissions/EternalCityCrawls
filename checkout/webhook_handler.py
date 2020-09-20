fda3138afec0192b38383861d4e92fe0d26aba77
Pierluca Del Buono <pierluca@amazon.com>
2020-09-20 11:20:48 +0200

    adding to the webhandler the check on user profile

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from .models import Order, OrderLineItem
 from tours.models import Tour
+from profiles.models import UserProfile
+
 
 import json
 import time
@@ -32,6 +34,20 @@ class StripeWH_Handler:
         billing_details = intent.charges.data[0].billing_details
         total = round(intent.charges.data[0].amount / 100, 2)
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = billing_details.phone
+                profile.default_country = billing_details.address.country
+                profile.default_postcode = billing_details.address.postal_code
+                profile.default_town_or_city = billing_details.address.city
+                profile.default_street_address1 = billing_details.address.line1
+                profile.default_street_address2 = billing_details.address.line2
+                profile.default_county = billing_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -39,6 +55,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=billing_details.name,
+                    user_profile=profile,
                     email__iexact=billing_details.email,
                     phone_number__iexact=billing_details.phone,
                     country__iexact=billing_details.address.country,
