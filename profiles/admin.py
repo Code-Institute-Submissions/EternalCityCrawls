edbbc66b351debaa19db7e15a46879e239ed56e6
Pierluca Del Buono <pierluca@amazon.com>
2020-09-27 13:29:05 +0200

    registering user profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'default_phone_number',
+        'default_town_or_city',
+        'default_street_address1',
+        'default_county',
+        'default_postcode',
+        'default_country',
+
+    )
+
+    ordering = ('user',)
+
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
