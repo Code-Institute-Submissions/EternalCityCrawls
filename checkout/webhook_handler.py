51203a29f84e964de9059fd05167e2a62524d1b3
Pierluca Del Buono <pierluca@amazon.com>
2020-09-27 19:24:04 +0200

    reducing mail body to test

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -25,7 +25,7 @@ class StripeWH_Handler:
             'checkout/confirmation_emails/subject.txt',
             {'order': order}
             )
-
+        print(1)
         body = render_to_string(
             'checkout/confirmation_emails/body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL}
